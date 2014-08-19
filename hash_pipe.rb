2988e562fd434cadf46f013f648e8b1a030d7e6b
Alex McRitchie <amcritchie@gmail.com>
2014-08-19 10:08:28 -0600

    Now the hashes are not over writen for every week.

--- hash_pipe.rb
+++ hash_pipe.rb
@@ -4,14 +4,30 @@ require 'awesome_print'
 students = {}
 
 CSV.foreach('data/gschool_commute_data.csv') do |row|
-  students[row[0]] = {
-      :week => row[1],
-      :day => row[2],
-      :mode => row[3],
-      :inbound => row[4],
-      :outbound => row[5],
-      :distance => row[6]
-  }
+  if students.has_key?(row[0])
+
+    students[row[0]].push({
+        :week => row[1],
+        :day => row[2],
+        :mode => row[3],
+        :inbound => row[4],
+        :outbound => row[5],
+        :distance => row[6]
+    })
+
+  else
+
+    students[row[0]] = Array.new(1)
+    students[row[0]][0] = {
+        :week => row[1],
+        :day => row[2],
+        :mode => row[3],
+        :inbound => row[4],
+        :outbound => row[5],
+        :distance => row[6]
+    }
+    
+  end
 end
 p '='*40
 ap students
