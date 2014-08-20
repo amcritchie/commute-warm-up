ca1285319627512f899abbe8148c5812f3499f0e
Alex McRitchie <amcritchie@gmail.com>
2014-08-20 10:26:49 -0600

    sorts by weeks then day of week.

--- hash_pipe.rb
+++ hash_pipe.rb
@@ -3,17 +3,37 @@ require 'awesome_print'
 
 students = {}
 
+
+def day_of_week(day)
+  if day == "Monday"
+    0
+  elsif day == "Tuesday"
+    1
+  elsif day =="Wednesday"
+    2
+  elsif day == "Thursday"
+    3
+  elsif day =="Friday"
+    4
+  elsif day == "Saturday"
+    5
+  elsif day =="Sunday"
+    6
+  end
+end
+
 CSV.foreach('data/gschool_commute_data.csv') do |row|
   if students.has_key?(row[0])
 
     students[row[0]].push({
-        :week => row[1],
-        :day => row[2],
-        :mode => row[3],
-        :inbound => row[4],
-        :outbound => row[5],
-        :distance => row[6]
-    })
+                              :week => row[1],
+                              :day => row[2],
+                              :mode => row[3],
+                              :inbound => row[4],
+                              :outbound => row[5],
+                              :distance => row[6]
+                          })
+
 
   else
 
@@ -29,5 +49,10 @@ CSV.foreach('data/gschool_commute_data.csv') do |row|
 
   end
 end
+
+students.each do |student|
+  student[1].sort_by! { |k| [k[:week], day_of_week(k[:day])] }
+end
+
 p '='*40
 ap students
